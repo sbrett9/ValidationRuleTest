59785df8f825a76afcd4751bf3a9b09284f53b3a
sulprizio_b <sbrett9@gmail.com>
2017-07-10 13:51:00 -0700

    foo

---
+++ MvvmLight22/MvvmLight22/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MvvmLight22
+{
+    #region Using Declarations
+
+    using System.Windows;
+    using GalaSoft.MvvmLight.Threading;
+
+    #endregion
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+    }
+}
